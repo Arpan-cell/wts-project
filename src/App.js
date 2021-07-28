e2a4cd33fcbfe65397b2f78aae3e83325338b69a
Arpan Roy <83748493+Arpan-cell@users.noreply.github.com>
2021-07-28 14:45:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+import Form from './Components/form/Form'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
